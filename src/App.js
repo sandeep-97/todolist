083a3e11f80246d6b2e6794438e8e9e365bcc3fe
sandeep-97 <menakuru.sandeep@gmail.com>
2021-03-08 20:11:22 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { content } from './components/layout/content';
+import { Header } from './components/layout/HEADER'
+import { Content } from './components/layout/content';
+export const App = () => (
+  <div className="App">
+    <Header></Header>
+    <Content></Content>
+  </div>
+);
---
+++ src/components/layout/content.js
@@ -0,0 +1,10 @@
+import { React } from "react";
+import { Sidebar } from "./sidebar";
+
+export const Content = () => {
+    return (
+        <section>
+            <Sidebar />
+        </section>
+    )
+}
